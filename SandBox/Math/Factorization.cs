96a1ec8dc3780f59099bb75ce67ff5673cafd419
iegor.osaulenko <iosaulenko@luxoft.com>
2018-10-29 12:25:26 +0200

    [PrimeFactorization] PrimesFactorisation & Serialization helper
    1. Included Factorization and cumulative memorization of prime factors list;
    2. Added generic helper to serialize working objects;

---
+++ SandBox/Math/Factorization.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SandBox.Math
+{
+    public class Factorization : LocalSave<List<int>>
+    {
+        private List<int> Primes => _obj;
+        public Factorization() : base(new List<int> { 2 }, "primes")
+        {
+            Load();
+        }
+
+        public async Task<int> GetPrime(int i)
+        {
+            var needSave = false;
+            while (Primes.Count < i)
+            {
+                GetNextPrime();
+                if (!needSave) needSave = true;
+            }
+
+            if(needSave) await Save().ConfigureAwait(true);
+            return Primes[i - 1];
+        }
+
+        private void GetNextPrime()
+        {
+            var n = Primes.Last() + 1;
+            while (!IsPrime(n))
+                n++;
+            Primes.Add(n);
+        }
+
+        private bool IsPrime(int n)
+            => Primes.FirstOrDefault(x => n % x == 0) == 0;
+    }
+}
---
+++ SandBox/Math/LocalSave.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading.Tasks;
+
+namespace SandBox.Math
+{
+    public abstract class LocalSave<T>
+    {
+        public T _obj { get; private set; }
+        public string _fileName { get; }
+
+        public string GetFullName => _fileName + ".bin";
+
+        protected LocalSave(T obj, string fileName)
+        {
+            _obj = obj;
+            _fileName = fileName;
+        }
+
+        public async Task Save()
+        {
+            if (File.Exists(GetFullName))
+                File.Delete(GetFullName);
+
+            using (Stream stream = new FileStream(GetFullName,
+                FileMode.Create,
+                FileAccess.Write,
+                FileShare.None))
+            {
+                IFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, _obj);
+            }
+        }
+
+        public void Load()
+        {
+            if (File.Exists(GetFullName))
+            {
+                using (Stream stream = new FileStream(GetFullName,
+                    FileMode.Open,
+                    FileAccess.Read,
+                    FileShare.Read))
+                {
+                    IFormatter formatter = new BinaryFormatter();
+                    _obj = (T)formatter.Deserialize(stream);
+                }
+            }
+        }
+    }
+}
