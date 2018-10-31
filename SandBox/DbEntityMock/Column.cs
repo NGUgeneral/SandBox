da2133f4adc46afeaf41cb126838e4da2f662511
iegor.osaulenko <iosaulenko@luxoft.com>
2018-10-31 16:56:39 +0200

    [DbMock] Included Table/Column structure entities

---
+++ SandBox/DbEntityMock/Column.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace SandBox.DbEntityMock
+{
+    public class Column<T>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public Table Parent { get; set; }
+        public T Value { get; set; }
+        public List<T> Values { get; set; }
+    }
+}
