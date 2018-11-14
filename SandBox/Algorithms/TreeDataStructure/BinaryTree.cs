089e48927fc1876f3e775ad664db73027e0a96c6
iegor.osaulenko <iosaulenko@luxoft.com>
2018-11-14 16:01:24 +0200

    [TreeDataStructure] Outlined the base for BinaryTree;

---
+++ SandBox/Algorithms/TreeDataStructure/BinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SandBox.Algorithms.TreeDataStructure
+{
+    public class BinaryTree<T> where T : IComparable
+    {
+        public BinaryNode<T> Root { get; private set; }
+
+        public BinaryTree()
+        {
+            Root = new BinaryNode<T>();
+        }
+
+        public BinaryTree(BinaryNode<T> root)
+        {
+            Root = root;
+        }
+
+        public BinaryTree(T value)
+        {
+            Root = new BinaryNode<T>
+            {
+                Value = value
+            };
+        }
+
+        public void InsertValue(T value)
+        {
+            var node = new BinaryNode<T>
+            {
+                Value = value
+            };
+
+            InsertNode(node);
+        }
+
+        public void InsertNode(BinaryNode<T> node)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
