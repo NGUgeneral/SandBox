30753d06f29f69aa296d25c196ac606f780d3a27
iegor.osaulenko <iosaulenko@luxoft.com>
2018-11-15 14:14:05 +0200

    [Codewars]
    SOLVED: https://www.codewars.com/kata/number-of-trailing-zeros-of-n/train/csharp

---
+++ CodewarsKatas/CodewarsKatas/Kyu5/NumberOfTrailingZeros.cs
@@ -0,0 +1,16 @@
+﻿namespace CodewarsKatas.CodewarsKatas.Kyu5
+{
+    class NumberOfTrailingZeros
+    {
+        //https://www.codewars.com/kata/number-of-trailing-zeros-of-n/train/csharp
+        public static int Run(int n)
+        {
+            var result = 0;
+
+            for (int i = 5; i <= n; i *= 5)
+                result += n / i;
+
+            return result;
+        }
+    }
+}
---
+++ CodewarsKatas/CodewarsKatas/Tests/Kyu5/NumberOfTrailingZerosTest.cs
@@ -0,0 +1,22 @@
+﻿using CodewarsKatas.CodewarsKatas.Kyu5;
+using NUnit.Framework;
+
+namespace CodewarsKatas.CodewarsKatas.Tests.Kyu5
+{
+    [TestFixture]
+    class NumberOfTrailingZerosTest
+    {
+        [Test]
+        [TestCase(0, 1)]
+        [TestCase(1, 5)]
+        [TestCase(2, 10)]
+        [TestCase(2, 12)]
+        [TestCase(3, 15)]
+        [TestCase(6, 25)]
+        [TestCase(131, 531)]
+        public void BasicTests(int expected, int n)
+        {
+            Assert.AreEqual(expected, NumberOfTrailingZeros.Run(n));
+        }
+    }
+}
