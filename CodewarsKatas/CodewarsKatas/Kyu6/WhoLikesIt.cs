ccbe601a28597ee9d44288f3e3203fec4f397cd9
egor.osaulenko <osaulenko@skyorca.net>
2018-12-03 18:55:36 +0200

    [Codewars] SOLVED: https://www.codewars.com/kata/who-likes-it/

---
+++ CodewarsKatas/CodewarsKatas/Kyu6/WhoLikesIt.cs
@@ -0,0 +1,26 @@
+﻿namespace CodewarsKatas.CodewarsKatas.Kyu6
+{
+  //https://www.codewars.com/kata/who-likes-it/
+  class WhoLikesIt
+  {
+    public static string Run(string[] names)
+      => names != null ? GetNamesSequence(names) + " like" + (names.Length > 1 ? string.Empty : "s") + " this" : null;
+
+    private static string GetNamesSequence(string[] names)
+    {
+      switch (names.Length)
+      {
+          case 0:
+            return "no one";
+          case 1:
+            return names[0];
+          case 2:
+            return names[0] + " and " + names[1];
+          case 3:
+            return names[0] + ", " + names[1] + " and " + names[2];
+          default:
+            return names[0] + ", " + names[1] + " and " + (names.Length - 2) + " others";
+      }
+    }
+  }
+}
---
+++ CodewarsKatas/CodewarsKatas/Tests/Kyu6/WhoLikesItTest.cs
@@ -0,0 +1,20 @@
+﻿using CodewarsKatas.CodewarsKatas.Kyu6;
+using NUnit.Framework;
+
+namespace CodewarsKatas.CodewarsKatas.Tests.Kyu6
+{
+  [TestFixture]
+  class WhoLikesItTest
+  {
+    [Test, Description("It should return correct text")]
+    [TestCase("no one likes this", new string[0])]
+    [TestCase("Peter likes this", new [] {"Peter"})]
+    [TestCase("Jacob and Alex like this", new [] {"Jacob", "Alex"})]
+    [TestCase("Max, John and Mark like this", new [] {"Max", "John", "Mark"})]
+    [TestCase("Alex, Jacob and 2 others like this", new [] {"Alex", "Jacob", "Mark", "Max"})]
+    public void SampleTest(string expected, string[] input)
+    {
+      Assert.AreEqual(expected, WhoLikesIt.Run(input));
+    }
+  }
+}
