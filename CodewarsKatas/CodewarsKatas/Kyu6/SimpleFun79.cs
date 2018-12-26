438c01d46dfd1a4f4d3a113bcbc520e79627425d
egor.osaulenko <osaulenko@skyorca.net>
2018-12-26 12:55:02 +0200

    [Codewars] Solution: SimpleFun79
    https://www.codewars.com/kata/simple-fun-number-79-delete-a-digit/train/csharp

---
+++ CodewarsKatas/CodewarsKatas/Kyu6/SimpleFun79.cs
@@ -0,0 +1,21 @@
+﻿namespace CodewarsKatas.CodewarsKatas.Kyu6
+{
+  class SimpleFun79
+  {
+    // https://www.codewars.com/kata/simple-fun-number-79-delete-a-digit/train/csharp
+    public static int DeleteDigit(int n)
+    {
+      var result = 0;
+      var num = n.ToString();
+
+      for (int i = 0; i < num.Length; i++)
+      {
+        var temp = int.Parse(num.Substring(0, i) + num.Substring(i + 1));
+        if (temp > result)
+          result = temp;
+      }
+
+      return result;
+    }
+  }
+}
---
+++ CodewarsKatas/CodewarsKatas/Tests/Kyu6/SimpleFun79Test.cs
@@ -0,0 +1,27 @@
+﻿using CodewarsKatas.CodewarsKatas.Kyu6;
+using NUnit.Framework;
+
+namespace CodewarsKatas.CodewarsKatas.Tests.Kyu6
+{
+  [TestFixture]
+  class SimpleFun79Test
+  {
+    [Test]
+    public void BasicTests1()
+    {
+      Assert.AreEqual(52,SimpleFun79.DeleteDigit(152));
+    }
+
+    [Test]
+    public void BasicTests2()
+    {
+      Assert.AreEqual(101,SimpleFun79.DeleteDigit(1001));
+    }
+
+    [Test]
+    public void BasicTests3()
+    {
+      Assert.AreEqual(1,SimpleFun79.DeleteDigit(10));
+    }
+  }
+}
