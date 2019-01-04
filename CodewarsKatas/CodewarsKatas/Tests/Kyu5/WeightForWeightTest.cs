e8e4ef244100f7e38f0a893b0fc9f14725dab040
egor.osaulenko <osaulenko@skyorca.net>
2019-01-04 13:26:07 +0200

    [Codewars] WIP: WeightForWeight

---
+++ CodewarsKatas/CodewarsKatas/Tests/Kyu5/WeightForWeightTest.cs
@@ -0,0 +1,16 @@
+﻿using CodewarsKatas.CodewarsKatas.Kyu5;
+using NUnit.Framework;
+
+namespace CodewarsKatas.CodewarsKatas.Tests.Kyu5
+{
+    [TestFixture]
+    public class WeightForWeightTest
+    {
+        [TestCase("2000 103 123 4444 99", "103 123 4444 99 2000")]
+        [TestCase("11 11 2000 10003 22 123 1234000 44444444 9999", "2000 10003 1234000 44444444 9999 11 11 22 123")]
+        public void Test(string expected, string input)
+        {
+            Assert.AreEqual(expected, WeightForWeight.OrderWeight(input));
+        }
+    }
+}
---
+++ CodewarsKatas/CodewarsKatas/WIP/Kyu5/WeightForWeight.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.CodeDom;
+using System.Linq;
+
+namespace CodewarsKatas.CodewarsKatas.Kyu5
+{
+    //https://www.codewars.com/kata/weight-for-weight/train/csharp
+    public class WeightForWeight
+    {
+        public static string OrderWeight(string input)
+        {
+            var weightIntValues = input.Split().Select(x => new WeightInt(x)).OrderBy(x => x).Select(x => x.Value).ToArray();
+            return string.Join(" ", weightIntValues);
+        }
+
+        private static int Weight(string input)
+        {
+            var result = 0;
+
+            for (int i = 0; i < input.Length; i++)
+                result += (int)char.GetNumericValue(input[i]);
+
+            return result;
+        }
+
+        private class WeightInt : IComparable
+        {
+            private int Weight { get; }
+            public string Value { get; }
+
+            public WeightInt(string value)
+            {
+                Value = value;
+                Weight = Weight(value);
+            }
+
+            public int CompareTo(object obj)
+            {
+                var other = (WeightInt) obj;
+
+                if (other.Weight > this.Weight)
+                    return -1;
+                if (other.Weight < this.Weight)
+                    return 1;
+                if (int.Parse(other.Value) > int.Parse(this.Value))
+                    return -1;
+                if (int.Parse(other.Value) < int.Parse(this.Value))
+                    return 1;
+
+                return 0;
+            }
+        }
+    }
+}
